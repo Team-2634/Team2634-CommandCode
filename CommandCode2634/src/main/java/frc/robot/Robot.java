5ea62c7e2dd84fa50b8ebc0c4f400b5506486642
Crimson <121061785+Adri-nK@users.noreply.github.com>
2023-04-12 12:41:36 -0400

    even more

--- CommandCode2634/src/main/java/frc/robot/Robot.java
+++ CommandCode2634/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
@@ -8,12 +5,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
